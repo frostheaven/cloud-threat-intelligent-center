9abc2fe499cb8ea652fe413a501b59590a1f5f9e
frostheaven <1945449218@qq.com>
2023-11-01 19:56:09 +0800

    11.1

--- src/common/home/home.vue
+++ src/common/home/home.vue
@@ -1,41 +1,46 @@
 <template>
+    
     <div class="top-menu">
         <top-menu />
     </div>
     <div class="central-display">
         <el-container class="layout-container">
-            <el-aside width="200px">
+            <el-aside width="200px" style="background-color: #1d2025;">
                 <el-scrollbar>
-                    <el-menu :default-openeds="['2']" 
-                    :default-active="activeIndex"
-                    @select="handleSelect">
+                    <el-menu :default-openeds="['2']" :default-active="activeIndex" @select="handleSelect">
                         <el-menu-item index="report">
                             <template #title>
                                 <el-icon>
-                                    <document/>
-                                </el-icon>威胁报告
+                                    <document />
+                                </el-icon>
+                                威胁报告
                             </template>
                         </el-menu-item>
 
-                        <el-sub-menu index="2">
+                        <el-sub-menu index="threat-knowledge-base">
                             <template #title>
-                                <el-icon><Collection /></el-icon>威胁知识库
+                                <el-icon>
+                                    <collection />
+                                </el-icon>威胁知识库
                             </template>
                             <el-menu-item-group>
-                                <el-menu-item index="virus-family-lib"><el-icon><CaretRight /></el-icon>病毒家族库</el-menu-item>
-                                <el-menu-item index="apt-organize-lib"><el-icon><CaretRight /></el-icon>APT组织库</el-menu-item>
+                                <el-menu-item index="virus-family-lib"><el-icon>
+                                        <CaretRight />
+                                    </el-icon>病毒家族库</el-menu-item>
+                                <el-menu-item index="apt-organize-lib"><el-icon>
+                                        <CaretRight />
+                                    </el-icon>APT组织库</el-menu-item>
                             </el-menu-item-group>
                         </el-sub-menu>
-
-                        <el-menu-item index="3">
+                        <el-menu-item index="security-information">
                             <template #title>
                                 <el-icon>
                                     <edit-pen />
                                 </el-icon>安全资讯
                             </template>
                         </el-menu-item>
-                        
-                        <el-menu-item index="4">
+
+                        <el-menu-item index="intelligence-sandbox">
                             <template #title>
                                 <el-icon>
                                     <message-box />
@@ -43,21 +48,13 @@
                             </template>
                         </el-menu-item>
 
-                        <!-- <el-menu-item index="5">
-                            <template #title>
-                                <el-icon>
-                                    <data-line />
-                                </el-icon>漏洞透析
-                            </template>
-                        </el-menu-item>
-                        
                         <el-menu-item index="6">
                             <template #title>
                                 <el-icon>
-                                    <star />
-                                </el-icon>情报订阅
+                                    <office-building />
+                                </el-icon>情报公司
                             </template>
-                        </el-menu-item> -->
+                        </el-menu-item>
 
                     </el-menu>
                 </el-scrollbar>
@@ -76,81 +73,90 @@
             </el-container>
         </el-container>
     </div>
-   
+
 </template>
 
 <script lang="ts" setup>
 import TopMenu from '@common/TopMenu/TopMenu.vue'
 
 import { ref, getCurrentInstance } from 'vue'
-import { Document, Collection, MessageBox, EditPen, DataLine, Star, CaretRight } from '@element-plus/icons-vue'
+import { Document, MessageBox, Collection, EditPen, OfficeBuilding, Star, CaretRight } from '@element-plus/icons-vue'
 import { ElContainer, ElAside, ElSubMenu, ElScrollbar, ElMenuItem, ElIcon, ElMenuItemGroup, ElMenu, ElHeader, ElMain, ElTable, ElTableColumn } from 'element-plus'
 
 const activeIndex: string = "1"
 const { proxy }: any = getCurrentInstance()
 const handleSelect = (key: string, keyPath: string[]) => {
-    console.log('key:', key, '|| keyPath:', keyPath)
-    const router: any = proxy.$router
-    router.push({
-        name: key
-    })
+console.log('key:', key, '|| keyPath:', keyPath)
+const router: any = proxy.$router
+router.push({
+    name: key
+})
 }
 
 </script>
 
 <style scoped lang="scss">
 .top-menu {
-    height: $top-menu-height;
-    position: fixed;
-    width: 100%;
+height: $top-menu-height;
+width: 100%;
 }
 
 .central-display {
-    height: calc(100vh - $top-menu-height);
-    padding-top: $top-menu-height;
-    width: 100%;
+height: calc(100vh - $top-menu-height);
+position: fixed;
+width: 100%;
 }
 
 .layout-container {
-    height: 100%;
-    .el-header {
-        position: relative;
-        background-color: $common-bgc-dark;
-    }
-    .el-menu {
-        border-right: none;
-        .el-menu-item, :deep(.el-sub-menu__title) {
-            color: $font-color-light;
-            &:hover {
-                background-color: lighten($common-bgc-dark, 10);
-                color: lighten($font-color-light, 10);
-            }
-        }
-        :deep(.is-active) {
-            color: $font-hover-color!important;;
-        }
-        &, & > * {
-            background: $common-bgc-dark;
-        }
-        
-    }
-    .el-main {
-        padding: 0;
-        width: 100%;
-        padding: 1rem;
-        // & > * {
-        //     border-radius: 5%;
-        // }
-    }
-    .toolbar {
-        display: inline-flex;
-        align-items: center;
-        justify-content: center;
-        height: 100%;
-        right: 20px;
-        font-size: 1rem;
-        color: $font-hover-color;
-    }
+height: 100%;
+
+.el-header {
+    position: relative;
+    background-color: $common-bgc-dark;
 }
 
+.el-menu {
+    border-right: none;
+
+    .el-menu-item,
+    :deep(.el-sub-menu__title) {
+        color: $font-color-light;
+
+        &:hover {
+            background-color: lighten($common-bgc-dark, 10);
+            color: lighten($font-color-light, 10);
+        }
+    }
+
+    :deep(.is-active) {
+        color: $font-hover-color !important;
+        ;
+    }
+
+    &,
+    &>* {
+        background: $common-bgc-dark;
+    }
+
+}
+
+.el-main {
+    padding: 0;
+    width: 100%;
+    padding: 1rem;
+    // & > * {
+    //     border-radius: 5%;
+    // }
+}
+
+.toolbar {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    right: 20px;
+    font-size: 1rem;
+    color: $font-hover-color;
+}
+}
 </style>
